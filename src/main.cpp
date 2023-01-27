470d25e8a7359a5dd8f2cc5dde4438052a8d7d88
Breadleaf <bkhnapa@gmail.com>
2023-01-27 12:01:56 -0700

    added added file name through cli

--- src/main.cpp
+++ src/main.cpp
@@ -4,18 +4,34 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/IR/Module.h>
 
+#include <iostream>
+#include <string>
+
 using namespace llvm;
 
-int main() {
+int main(int argc, char **argv) {
+
+	// Check for incorrect number of cli arguments
+
+	if (argc > 2) {
+		std::cerr << "Usage: " << argv[0] << " [file name] (optional)\n";
+		return 1;
+	}
+
+	// Check for file name in cli arguments, if none use main
+
+	std::string fileName = "main";
+	if (argc == 2) { fileName = argv[1]; }
+
 	LLVMContext context;
 	IRBuilder<> builder(context);
 
-	// Create module called "main" and set target triple
+	// Create module with name from cli arguments or main by default
 
-	auto module = new Module("main", context);
+	auto module = new Module(fileName + std::string(".ll"), context);
 	module->setTargetTriple(sys::getDefaultTargetTriple());
 
-	// Create function "add" with two arguments of type int32 and return type int32
+	// Create function add with two arguments of type int32 and return type int32
 
 	auto addFunc = Function::Create(
 			FunctionType::get(builder.getInt32Ty(), {builder.getInt32Ty(), builder.getInt32Ty()}, false),
@@ -24,7 +40,7 @@ int main() {
 			module
 			);
 
-	// Create inner code of function "add" and return result
+	// Create inner code of function add and return result
 
 	auto addEntry = BasicBlock::Create(context, "entry", addFunc);
 	builder.SetInsertPoint(addEntry);
@@ -33,7 +49,7 @@ int main() {
 	auto addResult = builder.CreateAdd(addArg1, addArg2);
 	builder.CreateRet(addResult);
 
-	// Create function "main" with no arguments and return type int32
+	// Create function main with no arguments and return type void
 
 	auto mainFunc = Function::Create(
 			FunctionType::get(builder.getInt32Ty(), false),
@@ -42,7 +58,7 @@ int main() {
 			module
 			);
 
-	// Create inner code of function "main" and return result of function call to "add"
+	// Create inner code of function main and print result of call to add function
 
 	auto entry = BasicBlock::Create(context, "entry", mainFunc);
 	builder.SetInsertPoint(entry);
